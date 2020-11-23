41abad4c9f0102b14a9276d8763eabfe93571ee7
martinLeht <martin.lehtomaa@gmail.com>
2020-11-23 12:21:21 +0200

    Heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,10 @@
 import * as dotenv from "dotenv";
 
-dotenv.config();
-let path = `${__dirname}/../../../.env`;
-dotenv.config({ path: path });
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+    let path = `${__dirname}/../../../.env`;
+    dotenv.config({ path: path });
+}
 
 export const PORT = process.env.PORT;
 export const MONGO_URI = process.env.MONGO_URI;
